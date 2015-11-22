1a61d5b10e6de3081e30e64611a6ec6e9d2e65b7
Mahesh Casiraghi <mahesh@coffeestrap.com>
2015-11-22 09:35:49 -0500

    Added a Procfile.

--- index.js
+++ index.js
@@ -14,3 +14,45 @@ controllers(app);
 app.listen(config.server.port, function () {
 	console.log("Listening on " + config.server.port + "...");
 });
+
+
+// "use strict";
+
+
+// 	var twit = require("twitter");
+
+
+// 	var twitter = new twit({
+//     "consumer_key": "CXVNsTDohsJaIxl0cjpuLKXYr",
+//     "consumer_secret": "Y49dNi2NPN9vJaPS95QnRLslOqisEuC7v934lHOfN05cVjbtDB",
+//     "access_token_key": "2834545563-QYQqm8hnLPiU3eFyAD8SGtKhfIYW7gMp8fGh8Xd",
+//     "access_token_secret": "SUquQt3XC2ve3IIa8JbwMa4bsRCpZSJuCVKYAXLUTDBBT"
+// 	});
+
+// 	var number 	= 	5;
+// 	var	util 	=	require('util')
+
+
+
+
+
+// twitter.stream('statuses/filter', {track: 'hello'}, function(stream) {
+//   stream.on('data', function(tweet) {
+//     console.log(tweet.text);
+//   });
+ 
+//   stream.on('error', function(error) {
+//     throw error;
+//   });
+// });
+
+
+
+// twitter.get('search/tweets', {q: '#nowplaying', count: number, geocode: "37.781157,-122.398720,10000mi"}, function(error, tweets, response){
+//    console.log(tweets);
+// });
+
+
+
+
+

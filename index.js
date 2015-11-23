b4ee983794c944c34aba95b7694b892446d02654
Mahesh Casiraghi <mahesh@coffeestrap.com>
2015-11-23 16:24:16 -0500

    Improved twitter search API.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var express = require("express"),
 
 var app = express();
 
+
 app.use(cors());
 
 controllers(app);
@@ -16,43 +17,6 @@ app.listen((process.env.PORT || 5000), function () {
 	console.log("Listening on " + (process.env.PORT || 5000) + "...");
 });
 
-
-
-// "use strict";
-
-
-// 	var twit = require("twitter");
-
-
-// 	var twitter = new twit({
-//     "consumer_key": "CXVNsTDohsJaIxl0cjpuLKXYr",
-//     "consumer_secret": "Y49dNi2NPN9vJaPS95QnRLslOqisEuC7v934lHOfN05cVjbtDB",
-//     "access_token_key": "2834545563-QYQqm8hnLPiU3eFyAD8SGtKhfIYW7gMp8fGh8Xd",
-//     "access_token_secret": "SUquQt3XC2ve3IIa8JbwMa4bsRCpZSJuCVKYAXLUTDBBT"
-// 	});
-
-// 	var number 	= 	5;
-// 	var	util 	=	require('util')
-
-
-
-
-
-// twitter.stream('statuses/filter', {track: 'hello'}, function(stream) {
-//   stream.on('data', function(tweet) {
-//     console.log(tweet.text);
-//   });
- 
-//   stream.on('error', function(error) {
-//     throw error;
-//   });
-// });
-
-
-
-// twitter.get('search/tweets', {q: '#nowplaying', count: number, geocode: "37.781157,-122.398720,10000mi"}, function(error, tweets, response){
-//    console.log(tweets);
-// });
 
 
 
